06909f496d725f0e89141cfec2085bc6e24348aa
LeeWoojin-99 <lwj1316@naver.com>
2021-06-02 06:16:05 +0900

    modify 335 Page.

--- 335Page.py
+++ 335Page.py
@@ -1,66 +1,85 @@
-from itertools import permutations
-# 경우의 수를 구할 수 있는 라이브러리 permutations
+# 파이썬
+from collections import deque
 
+def check_map(map_data):
+    print("check_map function")
+    for y in range(len(map_data)):
+        for x in range(len(map_data)):
+            print(map_data[y][x], end=' ')
+        print()
+    print()
 
-def solution(n, weak, dist):
-	# 길이를 2배로 늘려서 '원형'을 일자 형태로 변형
-	length = len(weak)
-	for i in range(length):
-		weak.append(weak[i]+n)
-	#print("weak : {}".format(weak)) #TEST
+def can_move(cur1, cur2, new_board):
+    Y, X = 0, 1 # 
+    cand = [] # 이동할 수 있는 경우가 담긴 리스트
+    DELTAS = [(-1, 0), (1, 0), (0, 1), (0, -1)] # 이동에 쓰이는 데이터
 
-	answer = len(dist) + 1
-	# 왜 +1을 해준 것일까 ?
-	# 투입할 친구 수의 최솟값을 찾아야 하므로 len(dist)+1로 초기화
-	
-	for start in range(length):
-	# 0부터 length-1까지의 위치를 각각 시작점으로 설정
-		#print("start : %d"%start) #TEST
+    for dy, dx in DELTAS:
+        # nxt : 이동했을 때의 좌표
+        nxt1 = (cur1[Y] + dy, cur1[X] + dx)
+        nxt2 = (cur2[Y] + dy, cur2[X] + dx)
 
-		for friends in list(permutations(dist, len(dist))):
-			# 친구를 나열하는 모든 경우의 수 각각에 대하여 확인
-			#print("friends : {}".format(friends)) #TEST
+        if new_board[nxt1[Y]][nxt1[X]] == 0 and new_board[nxt2[Y]][nxt2[X]] == 0:
+            # 이동한 좌표가 안전한지 확인
+            cand.append((nxt1, nxt2)) # 안전하다면 cand에 추가
 
-			count = 1 # 투입할 친구의 수
-			position = weak[start] + friends[count-1] # 투입된 친구가 시작 지점인 weak[start]에서부터 점검할 수 있는 최대 거리
+    # 회전
+    if cur1[Y] == cur2[Y]: # 가로 방향 일 때
+        # y좌표가 같다면 가로 방향이다.
+        UP, DOWN = -1, 1 # 회전할 때 사용되는 데이터. 위로 회전, 아래로 회전
+        for d in [UP, DOWN]:
+            # 회전
+            if new_board[cur1[Y]+d][cur1[X]] == 0 and new_board[cur2[Y]+d][cur2[X]] == 0:
+                # 회전했을 때의 좌표가 안전한지 확인
+                cand.append((cur1, (cur1[Y]+d, cur1[X]))) # cur1을 중심으로 회전
+                cand.append((cur2, (cur2[Y]+d, cur2[X]))) # cur2를 중심으로 회전
+    else: # 세로 방향 일 때
+        LEFT, RIGHT = -1, 1
+        for d in [LEFT, RIGHT]:
+            if new_board[cur1[Y]][cur1[X]+d] == 0 and new_board[cur2[Y]][cur2[X]+d] == 0:
+                cand.append(((cur1[Y], cur1[X]+d), cur1))
+                cand.append(((cur2[Y], cur2[X]+d), cur2))
 
-			for index in range(start, start+length): # 시작점부터 모든 취약 지점을 확인
-				#print("index : %d"%index) #TEST
-				#print("now position : %d"%position) #TEST
-				#print("weak[index] : %d"%weak[index]) #TEST #TEST
+    return cand
 
-				if position < weak[index]:
-				# position에서 점검하고 있는 사람이 weak[index] 지점까지는 점검하지 못함
-					#print("position < weak[index]\n%d < %d"%(position, weak[index])) #TEST
-					count += 1 # 새로운 친구를 투입
+def solution(board):
+    # board 외벽 둘러싸기
+    N = len(board)
+    new_board = [[1] * (N+2) for _ in range(N+2)]
+    check_map(new_board)
+    for i in range(N):
+        for j in range(N):
+            new_board[i+1][j+1] = board[i][j]
+    check_map(new_board)
 
-					if count > len(dist): # 인원 부족
-						#print("인원 부족") #TEST
-						break
+    # 현재 좌표 위치 큐 삽입, 확인용 set
+    que = deque([((1, 1), (1, 2), 0)])
+    confirm = set([((1, 1), (1, 2))])
+    print(confirm)
 
-					position = weak[index] + friends[count-1]
-					# count += 1로 바뀐 새로운 친구 friends[count-1]이 weak[index]에서부터 점검을 시작하여 점검할 수 있는 최대 거리
-					#print("new position : %d"%position) #TEST
-				#print() #TEST
-			answer = min(answer, count)
-			#print("\n") #TEST
+    while que:
+        cur1, cur2, count = que.popleft()
+        # cur1, 2 : 로봇의 위치
+        # count : 시간
 
-	if answer > len(dist): return -1 # 인원 초과
+        if cur1 == (N, N) or cur2 == (N, N): return count
+        # 로봇이 N, N에 도착했다면 count를 리턴
 
-	return answer
-    
+        for nxt in can_move(cur1, cur2, new_board):
+            # 이동할 수 있는 로봇의 위치들이 담긴 리스트 자료형을 리턴한 can_move 함수
+            # nxt : 이동할 수 있는 위치
+            
+            # 이동할 수 있는 위치들을 모두 알아낸 후에 그 위치가 중복된 위치인지 확인해가며
+            # 중복된 위치가 아니라면 이동
 
-print("결과값 : %d"%solution(12, [1,5,6,10], [3,4]))
-#print(solution(12, [1,3,4,9,10], [3,5,7]))
+            if nxt not in confirm:
+                # 이동했던 위치는 confirm에 담기는데
+                # 이동할 수 있는 위치가 이동했던 위치인지 확인
+                que.append((*nxt, count+1))
+                # 언패킹을 통하여 3개의 데이터를 que에 append
+                # 다음에 이동할 데이터
+                confirm.add(nxt)
+                # 이동할 예정이니 다음에 중복되지 않도록 confirm에 츄가
+                # dictionary 자료형이라 append가 아닌 add 메서드를 사용
 
-'''
-n : 외벽의 길이
-weak : 취약 지점의 위치가 담긴 배열
-dist : 각 친구가 1시간 동안 이동할 수 있는 거리
-
-취약 지점을 점검하기 위해 보내야 하는 친구 수의 최소값을 구하시오.
-'''
-'''
-1 3 4 9 10 13 15 16 21 22
-1 3 4 9 10  1  3  4  9 10
-'''
+print(solution([[0,0,0,1,1],[0,0,0,1,0],[0,1,0,1,1],[1,1,0,0,1],[0,0,0,0,0]]))
